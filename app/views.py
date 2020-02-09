2d1eb1f1e96402620c8e056c42fe1aefcd8688b8
Artem Kononov <artem213101zse@gmail.com>
2020-02-09 17:17:27 +0300

    Improve menu

--- app/views.py
+++ app/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def get_menu_context():
+    return [
+        {'url': '/', 'name': 'Главная'},
+        {'url': '/profile/', 'name': 'Профиль'},
+        {'url': '/login/', 'name': 'Логин'},
+        {'url': '/register/', 'name': 'Регистрация'},
+    ]
 
+def stream_page(request):
+    context = {
+        'pagename': 'Главная',
+        'menu': get_menu_context(),
+    }
+    return render(request, 'pages/stream.html', context)
 
-def stream_page(req):
-    context = {}
-
-    return render(req, 'pages/stream.html', context)
