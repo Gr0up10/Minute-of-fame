ab58cc356a348c2fcd2e99ac168216ece3408a04
Васильев Андрей <rainbowshardofdeath@gmail.com>
2020-02-10 19:51:05 +0400

    Added register page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+from .forms import *
+
 
 # Create your views here.
+
+def login_page(request):
+    if request.method == 'POST':
+        login_form = LoginForm(request.POST)
+        if login_form.is_valid():
+            username = login_form.data['username']
+            password = login_form.data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                # messages.add_message(request, messages.SUCCESS, "Авторизация успешна")
+            else:
+                pass
+                # messages.add_message(request, messages.ERROR, "Неправильный логин или пароль")
+        else:
+            pass
+            # messages.add_message(request, messages.ERROR, "Некорректные данные в форме авторизации")
+    return redirect('registration/login')
+
+
+def register(request):
+    context = dict()
+    if request.method == 'POST':
+        form = RegisterFormView(request.POST)
+        context['form'] = form
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            my_password = form.cleaned_data.get('password1')
+            _user = authenticate(username=username, password=my_password)
+            if _user.is_active:
+                login(request, _user)
+                return render(request, 'index.html', context)
+        return render(request, 'registration/register.html', context)
+    else:
+        form = RegisterFormView()
+        context['form'] = form
+        return render(request, 'registration/register.html', context)
--- minute_of_fame/urls.py
+++ minute_of_fame/urls.py
@@ -17,11 +17,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.contrib.auth import views as auth_views
+from app.forms import RegisterFormView
+
 from django.conf.urls.static import static
 
 from app import views
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
-                  path('login/', auth_views.LoginView.as_view()),
+                  path('login/', views.login_page),
+                  path('register/', views.register),
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
