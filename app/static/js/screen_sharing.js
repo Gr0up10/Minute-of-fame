16baee4aedc344b073454b2a63f18bee9657af37
MapLegend <maplegend@mail.ru>
2020-02-20 19:30:56 +0000

    Fix streams

--- app/static/js/screen_sharing.js
+++ app/static/js/screen_sharing.js
@@ -1,86 +1,7 @@
 let connection = new RTCMultiConnection();
-
-// by default, socket.io server is assumed to be deployed on your own URL
+// v3.4.7 or newer
 connection.socketURL = 'http://localhost:9001/';
-connection.videosContainer = document.getElementById("stream-box");
-connection.onstream = function (event) {
-    var existing = document.getElementById(event.streamid);
-    if (existing && existing.parentNode) {
-        existing.parentNode.removeChild(existing);
-    }
-
-    event.mediaElement.removeAttribute('src');
-    event.mediaElement.removeAttribute('srcObject');
-    event.mediaElement.muted = true;
-    event.mediaElement.volume = 0;
-
-    var video = document.createElement('video');
-
-    try {
-        video.setAttributeNode(document.createAttribute('autoplay'));
-        video.setAttributeNode(document.createAttribute('playsinline'));
-    } catch (e) {
-        video.setAttribute('autoplay', true);
-        video.setAttribute('playsinline', true);
-    }
-
-    if (event.type === 'local') {
-        video.volume = 0;
-        try {
-            video.setAttributeNode(document.createAttribute('muted'));
-        } catch (e) {
-            video.setAttribute('muted', true);
-        }
-    }
-    video.srcObject = event.stream;
-
-    var width = innerWidth - 80;
-    var mediaElement = getHTMLMediaElement(video, {
-        title: event.userid,
-        buttons: ['full-screen'],
-        width: width,
-        showOnMouseEnter: false
-    });
-
-    connection.videosContainer.appendChild(mediaElement);
-
-    setTimeout(function () {
-        mediaElement.media.play();
-    }, 5000);
-
-    mediaElement.id = event.streamid;
-
-    connection.onstreamended = function (event) {
-        var mediaElement = document.getElementById(event.streamid);
-        if (mediaElement) {
-            mediaElement.parentNode.removeChild(mediaElement);
-
-            if (event.userid === connection.sessionid && !connection.isInitiator) {
-                alert('Broadcast is ended. We will reload this page to clear the cache.');
-                location.reload();
-            }
-        }
-    };
-
-    connection.onMediaError = function (e) {
-        if (e.message === 'Concurrent mic process limit.') {
-            if (DetectRTC.audioInputDevices.length <= 1) {
-                alert('Please select external microphone. Check github issue number 483.');
-                return;
-            }
-
-            var secondaryMic = DetectRTC.audioInputDevices[1].deviceId;
-            connection.mediaConstraints.audio = {
-                deviceId: secondaryMic
-            };
-
-            connection.join(connection.sessionid);
-        }
-    };
-};
-
-// comment-out below line if you do not have your own socket.io server
-// connection.socketURL = 'https://rtcmulticonnection.herokuapp.com:443/';
+connection.videosContainer = document.getElementById('stream-box');
 
 connection.session = {
     screen: true,
@@ -99,22 +20,3 @@ document.getElementById('btn-connect').onclick = function () {
     this.disabled = true;
     connection.openOrJoin(room_id.value || 'predefined-roomid');
 };
-/*
-
-/!*
-// https://www.rtcmulticonnection.org/docs/iceServers/
-// use your own TURN-server here!
-connection.iceServers = [{
-    'urls': [
-        'stun:stun.l.google.com:19302',
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun.l.google.com:19302?transport=udp',
-    ]
-}];
-*!/
-
-
-
-
-*/
