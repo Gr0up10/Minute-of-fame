b1d3477677f5927f93332b6051f9baf1be82d4fe
Васильев Андрей <rainbowshardofdeath@gmail.com>
2020-02-25 16:27:17 +0400

    Added webcam-streaming button. JS-files update

--- app/static/js/streaming.js
+++ app/static/js/streaming.js
@@ -3,13 +3,8 @@ let connection = new RTCMultiConnection();
 connection.socketURL = 'http://localhost:9001/';
 connection.videosContainer = document.getElementById('stream-box');
 
-let room_id = document.getElementById('room_id');
+//let room_id = document.getElementById('room_id');
 //room_id.value = connection.token()
-
-// document.getElementById('btn-connect').onclick = function () {
-//     // this.disabled = true;
-//     connection.openOrJoin(room_id.value || 'predefined-roomid');
-// };
 
 function screen_stream() {
     connection.session = {
@@ -26,14 +21,18 @@ function screen_stream() {
 }
 
 function webcam_stream() {
-    connection.session = {
-        audio: true,
-        video: true
-    };
+    try {
+        connection.session = {
+            audio: true,
+            video: true
+        };
 
-    connection.sdpConstraints.mandatory = {
-        OfferToReceiveAudio: true,
-        OfferToReceiveVideo: true
-    };
-    connection.openOrJoin(room_id.value || 'predefined-roomid');
+        connection.sdpConstraints.mandatory = {
+            OfferToReceiveAudio: true,
+            OfferToReceiveVideo: true
+        };
+        connection.openOrJoin(room_id.value || 'predefined-roomid');
+    } catch (e) {
+        alert("Microphone or webcam not found");
+    }
 }
