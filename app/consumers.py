84cb42684e56f19840e542e961a3da1570e50177
Andrew Kravtsov <kravtandr@gmail.com>
2020-02-29 00:02:40 +0300

    Добавил сообщение между like/dislike и consumer

--- app/consumers.py
+++ app/consumers.py
@@ -3,20 +3,43 @@ import json
 
 
 class ChatConsumer(WebsocketConsumer):
+    likes = 0;
+    dislikes = 0;
+    poll_result = 0;
+
     def connect(self):
         self.accept()
+        print("Yes, connected")
 
     def disconnect(self, close_code):
-        pass
+        print("Disconnected")
+
 
     def receive(self, text_data):
         data = json.loads(text_data)
-        packet_name = data['command']
+        packet_name = data['message']
         self.packet_map[packet_name](self, data)
+        print("receive", data)
+
+
+
+
 
     def handle_message(self, packet):
-        print(packet)
+        print(packet,"=message")
+
+    def like(self, packet):
+        print(packet,"=like")
+        #self.likes += 1;
+        #self.poll_result = round(self.likes / (self.likes + self.dislikes) * 100);
+
+    def dislike(self, packet):
+        print(packet,"=dislike")
+        #self.dislikes += 1;
+        #self.poll_result = round(self.likes / (self.likes + self.dislikes) * 100);
 
     packet_map = {
-        "message": handle_message
+        "message": handle_message,
+        "like": like,
+        "dislike": dislike,
     }
--- app/routing.py
+++ app/routing.py
@@ -2,6 +2,34 @@ from django.urls import path
 
 from . import consumers
 
+
+'''
 websocket_urlpatterns = [
     path('ws/', consumers.ChatConsumer),
 ]
+'''
+from django.urls import path
+from django.urls import re_path
+
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from django.conf.urls import url
+from . import consumers
+
+
+
+websocket_urlpatterns = [
+    path('ws', consumers.ChatConsumer),
+    #url(r'^ws/chat$', consumers.ChatConsumer),
+    #url(r'^ws/chat$', consumers.ChatConsumer),
+    #re_path(r'ws/poll_websocket/test', consumers.ChatConsumer),
+]
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            websocket_urlpatterns
+        )
+    ),
+})
