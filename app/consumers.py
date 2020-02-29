8c31bef8734ce82a4a504176832eba0f5886440d
Andrew Kravtsov <kravtandr@gmail.com>
2020-02-29 16:18:08 +0300

    Голоcование на django channels, без redis

--- app/consumers.py
+++ app/consumers.py
@@ -1,15 +1,22 @@
+import channels.layers
+from channels.layers import get_channel_layer
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 import json
 
 
-class ChatConsumer(WebsocketConsumer):
+class PollConsumer(WebsocketConsumer):
     likes = 0;
     dislikes = 0;
     poll_result = 0;
-
+    channel_layer = get_channel_layer()
     def connect(self):
+        self.room='room'
+        self.room_group_name = 'ws_%s' % self.room
+        #print("channel= ", self.channel_layer, self.room_group_name)
         self.accept()
         print("Yes, connected")
+
 
     def disconnect(self, close_code):
         print("Disconnected")
@@ -19,27 +26,32 @@ class ChatConsumer(WebsocketConsumer):
         data = json.loads(text_data)
         packet_name = data['message']
         self.packet_map[packet_name](self, data)
-        print("receive", data)
-
-
-
+        #print("receive", data)
 
 
     def handle_message(self, packet):
-        print(packet,"=message")
+        print(packet,"= message_test is OK")
 
     def like(self, packet):
-        print(packet,"=like")
-        #self.likes += 1;
-        #self.poll_result = round(self.likes / (self.likes + self.dislikes) * 100);
+        self.likes += 1;
+        self.poll_result = round(self.likes / (self.likes + self.dislikes) * 100);
+        self.send(str(self.poll_result))
+        #print("likes = ", self.likes, "dislikes = ", self.dislikes, "res = ", self.poll_result)
+
 
     def dislike(self, packet):
-        print(packet,"=dislike")
-        #self.dislikes += 1;
-        #self.poll_result = round(self.likes / (self.likes + self.dislikes) * 100);
+        self.dislikes += 1
+        self.poll_result = round(self.likes / (self.likes + self.dislikes) * 100)
+        self.send(str(self.poll_result))
+        #print("likes = ",self.likes,"dislikes = ", self.dislikes,"res = ", self.poll_result)
+
+    def open(self, packet):
+        self.send(self.poll_result)
+        #print("open and send: ",self.poll_result)
 
     packet_map = {
         "message": handle_message,
         "like": like,
         "dislike": dislike,
+        "open": open,
     }
--- app/routing.py
+++ app/routing.py
@@ -1,17 +1,4 @@
 from django.urls import path
-
-from . import consumers
-
-
-'''
-websocket_urlpatterns = [
-    path('ws/', consumers.ChatConsumer),
-]
-'''
-from django.urls import path
-from django.urls import re_path
-
-
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from django.conf.urls import url
@@ -20,7 +7,7 @@ from . import consumers
 
 
 websocket_urlpatterns = [
-    path('ws', consumers.ChatConsumer),
+    path('ws', consumers.PollConsumer),
     #url(r'^ws/chat$', consumers.ChatConsumer),
     #url(r'^ws/chat$', consumers.ChatConsumer),
     #re_path(r'ws/poll_websocket/test', consumers.ChatConsumer),
