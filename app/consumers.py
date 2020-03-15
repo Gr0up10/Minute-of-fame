3ed50bc992085afaf9d0e7e7362dada8f5d5eacf
Васильев Андрей <rainbowshardofdeath@gmail.com>
2020-03-15 20:19:09 +0400

    fixed double messages view from sender

--- app/consumers.py
+++ app/consumers.py
@@ -149,12 +149,12 @@ class ChatConsumer(WebsocketConsumer):
         )
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        # text_data_json = json.loads(text_data)
+        # message = text_data_json['message']
+        #
+        # self.send(text_data=json.dumps({
+        #     'message': message
+        # }))
 
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
