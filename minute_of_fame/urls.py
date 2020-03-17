3ca328fa111f6edef4257f328666bbd83f44b54b
Veronika Dolgireva <dolgireva.nika@gmail.com>
2020-03-17 16:49:13 +0300

    test v1.1

--- minute_of_fame/urls.py
+++ minute_of_fame/urls.py
@@ -44,6 +44,7 @@ urlpatterns = [
          name='password_reset_complete'),
     path('', views.stream_page, name='index'),
     path('profile/', views.profile_page),
+    path('profile-settings/', views.profile_settings_page),
     path('about/', views.about_page),
     path('report/<int:badass_id>', views.report_page)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
