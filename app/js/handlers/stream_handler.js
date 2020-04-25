d56b782c767a392c3c31e3aae015eb0a4e5e12c9
Shutov Nikita <wigiglenekit@gmail.com>
2020-04-25 17:04:42 +0300

    added description on stream page added handlers (not tested)

--- app/js/handlers/stream_handler.js
+++ app/js/handlers/stream_handler.js
@@ -3,11 +3,14 @@ export default class StreamHandler {
         this.socket = socket;
         this.stream = stream;
 
-        stream.onstream = (act) => this.send('queue', act);
+        let name_input = document.getElementById("title_input")
+        let description_input = document.getElementById("description_input")
+
+        stream.onstream = (act) => this.send('queue', {'act': act , 'title': title_input , 'description': description_input});
     }
 
     handle_message(name, packet) {
-        if (name === "set_stream") this.stream.watchStream(packet.id);
+        if (name === "set_stream") this.stream.watchStream(packet.id, packet.title, packet.description);
         if (name === "stop") this.stream.stopStream();
         if (name === "update_places") console.log(packet);
         if (name === "set_time") console.log(packet);
