1bfacbcbf134faf66fa1beef3457b6118fa68593
Григорий Беликов <belikovgrisha9@gmail.com>
2020-05-16 11:36:39 +0300

    Cleaned up emote list display, toggle added

--- app/views.py
+++ app/views.py
@@ -37,7 +37,7 @@ def stream_page(request):
                # 'Regform': RegisterFormView(),
                # 'Logform': LoginForm(),
                'stream_id': ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(16)),
-               'emotes_names': [['Ricardo', '.png'], ['AbsoluteLegend', '.png'], ['ThumbUp', '.png'],
+               'emotes_list': [['Ricardo', '.png'], ['AbsoluteLegend', '.png'], ['ThumbUp', '.png'],
                                 ['SmugDance', '.gif'], ['Doge', '.png'], ['DogeDS', '.gif'], ['LatchBall', '.gif'],
                                 ['Cry', '.png'], ['HamsterCam', '.png'], ['JudgeLook', '.png']]
                }
