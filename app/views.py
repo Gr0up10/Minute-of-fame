d819de8fc6a95cad9a89d638b400382b68069614
Степанов Николай <stepanovnic.03@yandex.ru>
2020-05-25 12:12:34 +0300

    add top's backend part

--- app/views.py
+++ app/views.py
@@ -38,23 +38,51 @@ def get_menu_context():
 
 
 def top_page(req):
-    random_users = []
-    for i in range(50):
-        random_users.append(
-            [''.join([random.choice(string.ascii_letters + string.digits) for n in range(10)]),
-             random.randint(0, 1000),
-             random.randint(0, 1000),
-             random.randint(0, 10000)
-             ])
-    random_users = sorted(random_users, key=lambda x: x[3], reverse=True)    # Сортировка по кол-ву просмотров
-    indexes = [[i+1] for i in range(len(random_users))]     # Получение индексов для оттображения в топе
-    random_users = [x+y for x, y in zip(indexes, random_users)]
-
+    users = []
+    all_users = [[0, i] for i in User.objects.all()]
+    for user in all_users:
+        streams = Stream.objects.filter(publisher=user[1])
+        for stream in streams:
+            user[0] += len(StreamView.objects.filter(stream=stream))
+    all_users.sort(reverse=True)
+    if len(all_users) < 10:
+        for i in range(len(all_users)):
+            likes_count = 0
+            dislikes_count = 0
+            if len(PollStat.objects.filter(user=all_users[i][1])) > 0:
+                for k in PollStat.objects.filter(user=all_users[i][1]):
+                    if k.vote == 1:
+                        likes_count += 1
+                    else:
+                        dislikes_count += 1
+            users.append([i + 1, all_users[i][1].username, likes_count, dislikes_count, all_users[i][0]])
+    elif len(all_users) < 40:
+        for i in range(10):
+            likes_count = 0
+            dislikes_count = 0
+            if len(PollStat.objects.filter(user=all_users[i][1])) > 0:
+                for k in PollStat.objects.filter(user=all_users[i][1]):
+                    if k.vote == 1:
+                        likes_count += 1
+                    else:
+                        dislikes_count += 1
+            users.append([i + 1, all_users[i][1].username, likes_count, dislikes_count, all_users[i][0]])
+    else:
+        for i in range(40):
+            likes_count = 0
+            dislikes_count = 0
+            if len(PollStat.objects.filter(user=all_users[i][1])) > 0:
+                for k in PollStat.objects.filter(user=all_users[i][1]):
+                    if k.vote == 1:
+                        likes_count += 1
+                    else:
+                        dislikes_count += 1
+            users.append([i + 1, all_users[i][1].username, likes_count, dislikes_count, all_users[i][0]])
     # Формат передачи ин-фы о пользователе:
     # [*Номер в топе*, *Ник*, *Общее кол-во лайков*, *Общее кол-во дизлайков*, *Общее кол-во просмотров*]
 
     context = {"pagename": "Топ пользователей", 'menu': get_menu_context(),
-               "userbase": random_users
+               "userbase": users
                }
     return render(req, 'pages/top.html', context)
 
