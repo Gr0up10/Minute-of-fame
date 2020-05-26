87efbe9b91f061f4445e5e428b827ca8c06ecc4c
Степанов Николай <stepanovnic.03@yandex.ru>
2020-05-26 11:26:23 +0300

    fix sort of users

--- app/views.py
+++ app/views.py
@@ -37,6 +37,41 @@ def get_menu_context():
     ]
 
 
+def partition(arr, low, high):
+    i = (low - 1)  # index of smaller element
+    pivot = arr[high][0]  # pivot
+
+    for j in range(low, high):
+
+        # If current element is smaller than or
+        # equal to pivot
+        if arr[j][0] > pivot:
+            # increment index of smaller element
+            i = i + 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+
+# The main function that implements QuickSort
+# arr[] --> Array to be sorted,
+# low  --> Starting index,
+# high  --> Ending index
+
+# Function to do Quick sort
+def quickSort(arr, low, high):
+    if low < high:
+        # pi is partitioning index, arr[p] is now
+        # at right place
+        pi = partition(arr, low, high)
+
+        # Separately sort elements before
+        # partition and after partition
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+
 def top_page(req):
     users = []
     all_users = [[0, i] for i in User.objects.all()]
@@ -44,7 +79,7 @@ def top_page(req):
         streams = Stream.objects.filter(publisher=user[1])
         for stream in streams:
             user[0] += len(StreamView.objects.filter(stream=stream))
-    all_users.sort(reverse=True)
+    quickSort(all_users, 0, len(all_users) - 1)
     if len(all_users) < 10:
         for i in range(len(all_users)):
             likes_count = 0
